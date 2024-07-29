702cf78a265df73287dc36e729e3b626b1d6b38b
Crisandry Javier <crisandryj@gmail.com>
2024-07-29 13:13:11 -0400

    Feat: Add general form for name, email and number

--- src/General.jsx
+++ src/General.jsx
@@ -1,1 +1,27 @@
-function General() {}
+function General() {
+  return (
+    <>
+      <form action="">
+        <fieldset>
+          <legend>General</legend>
+          <ul>
+            <li>
+              <label for="name">Name:</label>
+              <input type="text" id="name" name="user_name" />
+            </li>
+            <li>
+              <label for="mail">Email:</label>
+              <input type="email" id="mail" name="user_email" />
+            </li>
+            <li>
+              <label for="number">Number:</label>
+              <input type="tel" id="number" name="user_num" />
+            </li>
+          </ul>
+        </fieldset>
+      </form>
+    </>
+  );
+}
+
+export default General;
