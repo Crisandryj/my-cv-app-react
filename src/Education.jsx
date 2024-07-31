99828f2da285d431ec0b4c622dcabceb476df80d
Crisandry Javier <crisandryj@gmail.com>
2024-07-31 09:11:06 -0400

    Duplicate general form in education jsx

---
+++ src/Education.jsx
@@ -0,0 +1,117 @@
+import { useState } from "react";
+
+function Education() {
+  //Track status of whether form has been submitted
+  const [submitted, setSubmitted] = useState(false);
+  //Save form inputs in object: Name,Email and Phone Number
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    number: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const myForm = document.getElementById("myForm");
+    const formData = new FormData(myForm);
+    const newForm = {
+      name: formData.get("user_name"),
+      email: formData.get("user_email"),
+      number: formData.get("user_num"),
+    };
+    setForm(newForm);
+    setSubmitted(true);
+  };
+
+  //copied submit for the moment
+  const onEdit = (e) => {
+    e.preventDefault();
+
+    setSubmitted(false);
+  };
+
+  function EditButton() {
+    return (
+      <button type="submit" onClick={onEdit}>
+        Edit
+      </button>
+    );
+  }
+  function SubmitButton() {
+    return (
+      <button type="submit" onClick={onSubmit}>
+        Submit
+      </button>
+    );
+  }
+  //Add a conditional statement - if submitted show edit button
+  function ClickButton() {
+    if (submitted == true) {
+      return <EditButton />;
+    } else {
+      return <SubmitButton />;
+    }
+  }
+  //on submit change to listed info without edit option
+  if (!submitted) {
+    return (
+      <>
+        <form action="" id="myForm">
+          <fieldset>
+            <legend>General</legend>
+            <ul>
+              <li>
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="user_name"
+                  defaultValue={form.name}
+                />
+              </li>
+              <li>
+                <label htmlFor="mail">Email:</label>
+                <input
+                  type="email"
+                  id="mail"
+                  name="user_email"
+                  defaultValue={form.email}
+                />
+              </li>
+              <li>
+                <label htmlFor="number">Number:</label>
+                <input
+                  type="tel"
+                  id="number"
+                  name="user_num"
+                  defaultValue={form.number}
+                />
+              </li>
+              <li className="button">
+                <ClickButton />
+              </li>
+            </ul>
+          </fieldset>
+        </form>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <fieldset>
+          <legend>General</legend>
+          <ul>
+            <li>{form.name}</li>
+            <li>{form.email}</li>
+            <li>{form.number}</li>
+            <li className="button">
+              <ClickButton />
+            </li>
+          </ul>
+        </fieldset>
+      </>
+    );
+  }
+}
+
+export default Education;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import General from "./General.jsx";
 import "./index.css";
+import Education from "./Education.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <General />
+    <Education />
   </React.StrictMode>
 );
