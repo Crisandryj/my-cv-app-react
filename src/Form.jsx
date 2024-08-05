f90701e93fa4e002cf0eb7ea3874306d32f8d252
Crisandry Javier <crisandryj@gmail.com>
2024-08-05 11:27:07 -0400

    Feat: Add index

--- src/Form.jsx
+++ src/Form.jsx
@@ -6,16 +6,19 @@ import React, { useState } from "react";
 function Form() {
   const [stack, setStack] = useState([]);
 
+  let index = 0;
   const onClick = (e) => {
     e.preventDefault();
-    const newEd = { src: <Education /> };
+    const newEd = { src: <Education key={index} /> };
     setStack([...stack, newEd]);
+    index = index + 1;
+    console.log(index);
   };
   return (
     <>
       <General />
       <Education />
-      {stack.map((x) => x.src)}
+      {stack.map((comp) => comp.src)}
       <button onClick={onClick}>+</button>
       <Experience />
     </>
