a53bd289602b2a3e202862b44480ed36ce3abdc6
Crisandry Javier <crisandryj@gmail.com>
2024-08-06 09:30:35 -0400

    Feat: Add remove btn for Education comps

--- src/Form.jsx
+++ src/Form.jsx
@@ -6,18 +6,26 @@ import { v4 as uuidv4 } from "uuid";
 
 function Form() {
   const [stack, setStack] = useState([]);
-  const onClick = (e) => {
+  const onClickAdd = (e) => {
     e.preventDefault();
     let index = 0;
     const newEd = { src: <Education key={uuidv4()} /> };
     setStack([...stack, newEd]);
   };
+
+  const onClickDelete = (e) => {
+    e.preventDefault();
+    setStack([...stack].splice(1));
+    console.log(stack);
+  };
+
   return (
     <div>
       <General />
       <Education key={uuidv4()} />
       {stack.map((comp) => comp.src)}
-      <button onClick={onClick}>+</button>
+      <button onClick={onClickAdd}>+</button>
+      <button onClick={onClickDelete}>-</button>
       <Experience />
     </div>
   );
